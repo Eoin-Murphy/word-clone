91bc5c1c6996d9aea0dda08bb99b6e7ad5c7dd89
Eoin <eoin@kicker-matchday.com>
2023-02-20 17:04:08 +0100

    wip

--- src/components/Game/InputGuess.js
+++ src/components/Game/InputGuess.js
@@ -4,15 +4,18 @@ function InputGuess({handleGuessSubmitted}) {
     const [guess, setGuess] = React.useState('');
 
     const handleLetterEntered = (e) => {
-        var guessValue =e.target.value.toUpperCase().replace(/[^A-Z]/g, '');
-        if(guessValue.length === 5){
-            handleGuessSubmitted(guessValue);
+        setGuess(e.target.value.toUpperCase().replace(/[^A-Z]/g, ''));
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(guess.length === 5){
+            handleGuessSubmitted(guess);
         }
-        setGuess(guessValue);
     }
 
     return (
-        <form className="guess-input-wrapper">
+        <form className="guess-input-wrapper" onSubmit={handleSubmit}>
             <label htmlFor="guessInput">Enter Guess:</label>
             <input value={guess} onChange={handleLetterEntered} id="guessInput" type="text" maxLength={5}></input>
         </form>
